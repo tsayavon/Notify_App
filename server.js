bb7138af6bcd61c91a30ebf2e21045d86641c9db
tsayavon <sayavongtiffany05@gmail.com>
2022-07-12 22:49:51 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+// Require dependencies
+
+// Initialize the express app
+
+// Configure app settings
+
+// Mount middleware
+
+// Mount routes
+
+// Tell the app to listen for requests from client
+
+// 
