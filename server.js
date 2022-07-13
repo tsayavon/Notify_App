26db2f86c92cd7f9b657d2a5782d58847f345a24
tsayavon <sayavongtiffany05@gmail.com>
2022-07-12 23:03:12 -0400

    add basic config

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 // Require dependencies
+const express = require ('express');
+
 
 // Initialize the express app
+const app = express();
 
 // Configure app settings
 
 // Mount middleware
 
 // Mount routes
+app.get('/', (req, res) => {
+    res.send('<h1> Hello World!</h1>');
+});
 
 // Tell the app to listen for requests from client
-
+app.listen(3000, () => {
+    console.log(`Express is listening for request on pot:3000`);
+});
 // 
